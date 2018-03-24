54724b858ff42a232213312a4d3f61ff909de63b
Sarah Pham <s.pham7@student.unimelb.edu.au>
2018-03-24 13:34:38 +1100

    Start project

---
+++ app/src/main/java/census/census_sms/Recieve_sms.java
@@ -0,0 +1,13 @@
+package census.census_sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recieve_sms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieve_sms);
+    }
+}
